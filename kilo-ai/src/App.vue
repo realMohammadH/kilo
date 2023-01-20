d79dfaf352123d6adfe14ad623d6eec3fb0058ba
realMohammadH <dev.mhanens@gmail.com>
2023-01-20 22:42:07 +0200

    jan 20

--- kilo-ai/src/App.vue
+++ kilo-ai/src/App.vue
@@ -1,29 +1,38 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { h } from "vue";
+
+const section = (props, { slots }) => {
+  return h(
+    "div",
+    {
+      class: ["section", props.className],
+      style: { padding: props.padding },
+    },
+    slots
+  );
+};
+
+const container = ({ slots }) => {
+  return h(
+    "div",
+    {
+      class: "container",
+      style: { width: "1200px" },
+    },
+    slots
+  );
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header></header>
+  <main>
+    <section className="hero" padding="80px 0">
+      <container></container>
+    </section>
+  </main>
+  <footer></footer>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
